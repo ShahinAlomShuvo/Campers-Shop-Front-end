0aa8ad2f79c25f597e3ba9ebff0971ee098e7bd6
ShahinAliShuvo <shahinalomshuvo94@gmail.com>
2024-09-22 21:55:14 +0600

    make responsive hero section

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
     <Swiper
       modules={[Pagination, Autoplay]}
       pagination={{ clickable: true }}
-      className="h-[90vh] w-full"
+      className="h-[60vh] md:h-[90vh] w-full "
       autoplay={{ delay: 5000, disableOnInteraction: false }}
       effect="fade"
       speed={800}
@@ -44,16 +44,20 @@ const Hero = () => {
             <img
               src={slide.img}
               alt="Slide image"
-              className="object-cover w-full h-full"
+              className="object-center w-full h-full"
             />
-            <div className="absolute bottom-40 right-10 bg-black bg-opacity-30 p-6 rounded-md">
-              <h2 className="text-xl text-white  mb-2">{slide.discount}</h2>
-              <p className="text-6xl text-white font-bold mb-4">
+            <div className="absolute bottom-6 right-6 md:bottom-10 md:right-10 lg:bottom-12 lg:right-12 bg-black bg-opacity-50 p-4 md:p-6 rounded-md">
+              <h2 className="text-lg md:text-xl lg:text-2xl text-white mb-2">
+                {slide.discount}
+              </h2>
+              <p className="text-2xl md:text-4xl lg:text-5xl text-white font-bold mb-4">
                 {slide.title}
               </p>
-              <p className="text-sm text-white mb-4">{slide.description}</p>
-              <Link to={"/products"}>
-                <button className="bg-white text-black py-2 px-4 rounded hover:bg-[#ffc107]  transition">
+              <p className="text-xs md:text-sm lg:text-base text-white mb-4">
+                {slide.description}
+              </p>
+              <Link to="/products">
+                <button className="bg-white text-black py-2 px-4 md:py-3 md:px-6 lg:py-4 lg:px-8 rounded hover:bg-[#ffc107] transition">
                   {slide.buttonText}
                 </button>
               </Link>
