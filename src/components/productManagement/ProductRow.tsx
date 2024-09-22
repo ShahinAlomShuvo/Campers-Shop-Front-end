ce2a5507322aad8ef8054ee18adfe19aade44bbe
ShahinAliShuvo <shahinalomshuvo94@gmail.com>
2024-09-23 04:21:36 +0600

    Implement Page reload warning feature

--- src/components/productManagement/ProductRow.tsx
+++ src/components/productManagement/ProductRow.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useDeleteProductMutation } from "@/redux/api/api";
 import EditProduct from "./EditProduct";
 import { FaTrashAlt } from "react-icons/fa";
